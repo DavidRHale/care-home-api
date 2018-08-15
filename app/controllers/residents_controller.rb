efdf5dac5fdd408a818cb018a6922b837a4c8b3d
David Hale <davidhale@davids-air.home>
2018-08-15 20:20:24 +0100

    add show for residents which returns resident when found

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,8 +1,19 @@
 class ResidentsController < ApplicationController
+  before_action :set_resident, only: [:show]
 
   def index
     @residents = Resident.all
     render json: @residents, status: :ok
+  end
+
+  def show
+    render json: @resident, status: :ok
+  end
+
+  private
+
+  def set_resident
+    @resident = Resident.find(params[:id])
   end
 
 end
--- spec/requests/residents_spec.rb
+++ spec/requests/residents_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Residents API', type: :request do
     end
   end
 
+  describe 'GET /residents/:id' do
+    before { get "/residents/#{resident_id}" }
+
+    context 'when the record exists' do
+      it 'returns the resident' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(resident_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+
 end
