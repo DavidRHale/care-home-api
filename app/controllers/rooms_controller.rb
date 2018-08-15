8c0323d2f49e87b1b7464e06885ba4ea56533d2c
David Hale <davidhale@davids-air.home>
2018-08-15 22:04:24 +0100

    add show to rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,19 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:show]
 
   def index
     @room = Room.all
     json_response(@room)
+  end
+
+  def show
+    json_response(@room)
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 
 end
--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe 'Rooms API' do
   let!(:room2) { create(:room, resident_id: resident2.id) }
   let!(:room3) { create(:room, resident_id: resident3.id) }
 
+  let(:room_id) { room1.id }
+
   describe 'GET /rooms' do
     before { get '/rooms' }
 
@@ -19,6 +21,33 @@ RSpec.describe 'Rooms API' do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+  
+  describe 'GET /rooms/:id' do
+    before { get "/rooms/#{room_id}" }
+
+    context 'when the record exists' do
+      it 'returns the room' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(room_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:room_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Room/)
+      end
     end
   end
 
